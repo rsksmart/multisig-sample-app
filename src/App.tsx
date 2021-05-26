75abf5243b1f798fef8a236c4cd97a0bf5ef4ae6
Jesse Clark <hello@developerjesse.com>
2021-05-26 13:35:28 +0300

    add rLogin

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import RLogin, { RLoginButton } from '@rsksmart/rlogin'
 import './App.scss'
+
+const rLogin = new RLogin({
+  cacheProvider: false,
+  supportedChains: [30, 31]
+})
 
 function App () {
   return (
@@ -7,6 +13,10 @@ function App () {
       <header className="App-header">
         RIF Multisig Sample App
       </header>
+      <section className="login">
+        <h2>Login with your wallet</h2>
+        <RLoginButton />
+      </section>
     </div>
   )
 }
