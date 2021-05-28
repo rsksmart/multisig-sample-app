494645dc478a7daa790b1d15e2d0c5cbd8d51335
Jesse Clark <hello@developerjesse.com>
2021-05-28 17:51:55 +0300

    rename type to table scss.'

--- src/components/SafeInteraction.tsx
+++ src/components/SafeInteraction.tsx
@@ -23,57 +23,33 @@ const SafeInteraction: React.FC<Interface> = ({ web3Provider, safe, handleLogout
     safe.getThreshold().then((result: number) => setThreshold(result))
   }, [safe])
 
-  const createTransaction = () => {
-    console.log('heheheheheh!')
-    const signer = new ethers.providers.Web3Provider(web3Provider).getSigner(0)
-    console.log('signer', signer)
-
-    EthersSafe.create(
-      ethers,
-      safeAddress,
-      signer
-    )
-      .then((signerSafe: EthersSafe) => {
-        console.log('signerSafe:', signerSafe)
-
-        signerSafe.createTransaction({
-          to: '0x3dd03d7d6c3137f1eb7582ba5957b8a2e26f304a',
-          value: '10000', // ethers.BigNumber.from('10000').toString(),
-          data: '0x'
-        })
-          .then((transaction: SafeTransaction) => {
-            console.log('transaction', transaction)
-            signerSafe.signTransaction(transaction)
-              .then((result: any) => {
-                console.log('it signed')
-                // signerSafe.)
-              })
-              .catch((err: Error) => console.log('error3', err))
-          })
-          .catch((err: Error) => console.log('error2', err))
-      })
-  }
-
   return (
     <section className="selectedSafe">
       <Navigation handleLogout={handleLogout} />
       <section className="panel">
         <h3>Safe Information</h3>
-        <dl>
-          <dt>Safe Address</dt>
-          <dd><ValueWithButtons value={safeAddress} /></dd>
-          <dt>Balance</dt>
-          <dd className="text">{(balance / 1000000000000000000).toString()}</dd>
-          <dt>Owners</dt>
-          {owners.map((owner: string) => <dd key={owner}><ValueWithButtons value={owner} /></dd>)}
-          <dt>Threshold</dt>
-          <dd className="text">{threshold}</dd>
-        </dl>
-      </section>
-
-      <section className="panel">
-        <h3>Transactions</h3>
-        <button onClick={createTransaction}>Create Transaction</button>
+        <table>
+          <tbody>
+            <tr>
+              <th>Safe Address</th>
+              <td><ValueWithButtons value={safeAddress} /></td>
+            </tr>
+            <tr className="text">
+              <th>Balance</th>
+              <td>{(balance / 1000000000000000000).toString()}</td>
+            </tr>
+            <tr>
+              <th>Owners</th>
+              <td>
+                {owners.map((owner: string) => <div key={owner} ><ValueWithButtons value={owner} /></div>)}
+              </td>
+            </tr>
+            <tr className="text">
+              <th>Threshold</th>
+              <td>{threshold}</td>
+            </tr>
+          </tbody>
+        </table>
       </section>
     </section>
   )
