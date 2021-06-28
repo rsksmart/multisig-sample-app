2f5ae212f3d18dc0c9d90ebaa7e23225fabab38a
Jesse Clark <hello@developerjesse.com>
2021-06-28 14:28:22 +0300

    Add version to footer and bump to 1.0.0.

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { version } from '../../package.json'
 
 const FooterComponent: React.FC = () => {
   return (
     <section className="footer">
       <p>This is a sample app and does not save transactions outside of the state. If you refresh the app, or log in with a different account, the transactions will be cleared.</p>
       <p>Copyright © 2021 IOV Labs. All rights reserved.</p>
+      <p>Version: {version}</p>
     </section>
   )
 }
